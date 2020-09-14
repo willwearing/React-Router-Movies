a76889f9f88aff6bcbed348639ff3258293a66fd
willwearing <willwearing@gmail.com>
2020-09-14 13:40:10 -0600

    server setup and running ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .get("http://localhost:5000/api/movies") // Study this endpoint with Postman
+        .then((response) => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
+    };
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
+  const addToSavedList = (id) => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList
+        list={
+          [
+            /* This is stretch */
+          ]
+        }
+      />
 
       <div>Replace this Div with your Routes</div>
     </div>
